e86c9dfe735b692daf285d35cca10f96a8c19629
John Resig <jeresig@gmail.com>
2017-04-23 19:11:02 -0400

    Add registration for the custom logo.

--- functions.php
+++ functions.php
@@ -5,4 +5,11 @@ function register_my_menu() {
 }
 add_action( 'init', 'register_my_menu' );
 
+add_theme_support( 'custom-logo', array(
+	'height'      => 48,
+	'width'       => 48,
+	'flex-height' => false,
+	'flex-width'  => false,
+) );
+
 ?>
